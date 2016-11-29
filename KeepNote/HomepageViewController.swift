222a357d0f456ffa80c2eed01ed2bb3b06b40e2b
Meryl Barantal <meryl2194@gmail.com>
2016-11-29 18:03:04 +0100

    collectionView, singleCellView

--- KeepNote/HomepageViewController.swift
+++ KeepNote/HomepageViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class HomepageViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    let reuseIdentifier = "NoteCell" // also enter this string as the cell identifier in the storyboard
+    var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"]
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,23 +26,27 @@ class HomepageViewController: UIViewController, UICollectionViewDelegate, UIColl
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
+        return self.items.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return nil
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! NoteCollectionViewCell
+        
+        // Use the outlet in our custom class to get a reference to the UILabel in the cell
+        cell.cellLabel.text = self.items[indexPath.item]
+        cell.backgroundColor = UIColor.yellow // make cell more visible in our example project
+        return cell
     }
     
-    collectionView
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // handle tap events
+        print("You selected cell #\(indexPath.item)!")
     }
-    */
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // The first cell always start expanded with an height of 300
+    
+            return CGSize(width: collectionView.bounds.size.width, height: 300)
+    }
 
 }
---
+++ KeepNote/NoteCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  NoteCollectionViewCell.swift
+//  KeepNote
+//
+//  Created by Meryl Barantal on 29/11/2016.
+//  Copyright © 2016 com.github.keena.shemana. All rights reserved.
+//
+
+import UIKit
+
+class NoteCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var cellLabel: UILabel!
+}
