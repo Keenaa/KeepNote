36c6a2fe00f50ff7b80e1ecced61ada01fecd8a0
Guillaume Chieb Bouares <guillaume@horsecom.io>
2016-12-04 02:43:37 +0100

    added remove & update + refactor readNotes()

--- KeepNote/NoteManager.swift
+++ KeepNote/NoteManager.swift
@@ -14,17 +14,25 @@ public class NoteManager {
     
     public static let sharedInstance = NoteManager()
     
-    func createNote(id:Int,title:String,content:String,position:Int,color:UIColor) -> Note? {
+    public var currentNote:Note? {
+        didSet {
+            updateNote()
+        }
+    }
+    
+    public var noteList:[Note]?
+    
+    func createNote(title:String,content:String,position:Int,color:UIColor) -> Note? {
         if let context =  DataManager.sharedInstance.objectContext {
             let note = Note(context: context)
-            note.id = Int32(id)
             note.title = title
+            
             note.content = content
             note.position = Int32(position)
             note.color = color
             note.created_at = Date() as NSDate?
             do {
-                try? context.save()
+                try context.save()
                 return note
             } catch {
                 print("failure : \(error)")
@@ -33,12 +41,42 @@ public class NoteManager {
         }
         return nil
     }
-    func readNotes() -> [Note] {
+    func readNotes() -> Bool {
         if let context = DataManager.sharedInstance.objectContext {
             let request: NSFetchRequest<Note> = Note.fetchRequest()
             if let notes = try? context.fetch(request) {
-                return notes
+                self.noteList = notes
+                return true
             }
+        }
+        return false
+    }
+    private func readNoteById(context: NSManagedObjectContext,id:NSManagedObjectID) -> Note {
+        return context.object(with: id) as! Note
+    }
+    /*
+     * Update the current object in Note Manager
+     * just need to set the current object to update
+     */
+    private func updateNote() -> Void {
+        if let context = DataManager.sharedInstance.objectContext {
+            let note = readNoteById(context: context, id: (currentNote?.objectID)!)
+            note.title = currentNote?.title
+            
+            note.content = currentNote?.content
+            note.position = Int32((currentNote?.position)!)
+            note.color = currentNote?.color
+            do{
+                try context.save()
+            } catch let error as NSError {
+                print(error)
+            }
+        }
+    }
+    func remove(note:Note) -> Void {
+        if let context = DataManager.sharedInstance.objectContext {
+            let note = readNoteById(context: context, id: note.objectID)
+            context.delete(note)
         }
     }
 }
